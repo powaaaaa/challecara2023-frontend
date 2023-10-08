73329b0e384f4aac0eb0ade14ab89ed97ef04348
powaaaaa <knyuki2525@gmail.com>
2023-10-08 18:33:45 +0900

    chore: add extend theme to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        base: '#F6F1F1',
+        sub: '#AFD3E2',
+        main: '#19A7CE',
+        accent: '#146C94',
+        white: '#FAFAFA',
+        black: {
+          DEFAULT: '#3C3D44',
+          lighten: {
+            1: '#656464',
+            2: '#C2C2C2',
+          },
+        },
+      },
       fontFamily: {
         NotoSans: ['var(--font-noto-sans)'],
         NotoSansJP: ['var(--font-noto-sans-jp)'],
