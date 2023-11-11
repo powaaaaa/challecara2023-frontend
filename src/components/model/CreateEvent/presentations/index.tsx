b8cf687b9323fe2bc79baaf12afdcd218d9ceb53
powaaaaa <knyuki2525@gmail.com>
2023-11-11 18:17:52 +0900

    feat: use flex at createEvent

--- src/components/model/CreateEvent/presentations/index.tsx
+++ src/components/model/CreateEvent/presentations/index.tsx
@@ -13,39 +13,72 @@ export const CreateEventPresentation: React.FC = () => (
     <div className="mt-16 mr-10 ml-20">
       <div className="flex flex-col">
         <div className="flex">
-          <label className="flex-none w-[134px]" htmlFor="id">
+          <label
+            className="flex-none w-[134px] text-xl self-center"
+            htmlFor="id"
+          >
             タイトル
           </label>
           <Input type="text" display="hidden" label="label" id="id" />
         </div>
-        <div className="flex">
-          <label className="flex w-[134px]" htmlFor="button">
+
+        <div className="flex pt-2">
+          <label
+            className="flex w-[134px] text-xl self-center"
+            htmlFor="button"
+          >
             タグ
           </label>
           <TagsButtonPresentation id="button" />
         </div>
       </div>
 
-      <div className="flex mt-10">
-        <div className="flex-none w-[134px]">
-          募集期間
-          <br />
-          募集人数
+      <div className="flex flex-col mt-10">
+        <div className="flex">
+          <label
+            className="flex-none w-[134px] text-xl self-center"
+            htmlFor="id2"
+          >
+            募集人数
+          </label>
+          <div className="mb-2">
+            <Input type="number" display="hidden" label="label" id="id2" />
+          </div>
         </div>
-        <div>
-          <Input type="number" display="hidden" label="label" id="id2" />
-          <DateInput onChange={(e): void => console.log(e)} />
+
+        <div className="flex">
+          <label
+            className="flex-none w-[134px] text-xl self-center"
+            htmlFor="id3"
+          >
+            募集期間
+          </label>
+          <div>
+            <DateInput id="id3" onChange={(e): void => console.log(e)} />
+          </div>
         </div>
       </div>
-      <div className="flex my-10">
-        <div className="flex-none w-[134px]">
-          詳細
-          <br />
-          イベントID
+
+      <div className="flex flex-col my-10">
+        <div className="flex">
+          <label
+            className="flex-none w-[134px] text-xl self-center"
+            htmlFor="id4"
+          >
+            詳細
+          </label>
+          <div className="mb-2">
+            <Input type="text" display="hidden" label="label" id="id4" />
+          </div>
         </div>
-        <div>
-          <Input type="text" display="hidden" label="label" id="id3" />
-          <Input type="text" display="hidden" label="label" id="id4" />
+        <div className="flex">
+          <label
+            className="flex-none w-[134px] text-xl self-center"
+            htmlFor="id5"
+          >
+            イベントID
+          </label>
+          <Input type="text" display="hidden" label="label" id="id5" />
         </div>
       </div>
     </div>
