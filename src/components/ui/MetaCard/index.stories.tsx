4ade3ca861e20d9237091abff26dd82c70d32485
powaaaaa <knyuki2525@gmail.com>
2023-12-01 05:35:59 +0900

    fix: type in story

--- src/components/ui/MetaCard/index.stories.tsx
+++ src/components/ui/MetaCard/index.stories.tsx
@@ -21,8 +21,8 @@ export const Default: Story = {
       'グルメフェア株式会社は、美味しさと幸せを提供することに情熱を傾ける食品会社です。',
     snsUrl: 'https://x.com/Elonmusk',
     homepageUrl: 'https://example.com',
-    startTime: new Date(),
-    endTime: new Date(),
+    startTime: '2019-08-24T14:15:22Z',
+    endTime: '2019-08-24T14:15:22Z',
     winningNumber: 10,
   },
 };
