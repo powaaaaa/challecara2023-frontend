9a51141afb1050657e1b18c65a63509188cd5d50
powaaaaa <knyuki2525@gmail.com>
2023-12-02 07:30:04 +0900

    feat: a

--- src/components/page/Top/index.tsx
+++ src/components/page/Top/index.tsx
@@ -1,14 +1,10 @@
 import { Top } from '@/components/model/Top';
-import { Back } from '@/components/ui/Back';
 import { Header } from '@/components/ui/Header';
 
 export const Screen: React.FC = () => (
   <div>
     <div className="mb-4">
       <Header />
-    </div>
-    <div className="ml-20">
-      <Back />
     </div>
     <div className="flex flex-col items-center">
       <Top />
