214069581221b5aacba5a362713e926c8a9a9087
powaaaaa <knyuki2525@gmail.com>
2023-12-01 19:34:50 +0900

    fix: image sec

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image';
 
-import Logo from '../../../../public/logo.png';
-
 export const Header: React.FC = () => (
   <div className="relative w-[1280px] h-[80px] bg-main">
     <div className="absolute left-[72px] top-2">
-      <Image src={Logo} alt="ロゴ" width={172} height={63} />
+      <Image src="/logo.png" alt="ロゴ" width={172} height={63} />
     </div>
   </div>
 );
